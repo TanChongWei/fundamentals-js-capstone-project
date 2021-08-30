1b9849527a1c94c0123e6e39af9a728d757185c6
chongwei <chongwei215@gmail.com>
2021-08-30 13:46:05 +0800

    refactor code, fix logic

--- index.js
+++ index.js
@@ -31,29 +31,23 @@
     }
 
     //extracts the parameters from the url to determine the comic numbers to fetch and the no. of images to show
-    function getParams(option = 'next') {
+    function getParams() {
         const params = new URLSearchParams(window.location.search)
         const toShow = parseInt(params.get('toShow'))
         let nextImg = parseInt(params.get('img'))
-        switch (option) {
-            case 'next':
-                nextImg += toShow
-                break
-            case 'prev':
-                nextImg -= toShow
-                break
-            case 'rand':
-                nextImg = Math.floor(Math.random() * 2505 + 1)
-                break
-        }
         return [toShow, nextImg]
+    }
+
+    function getDisplayedImages() {
+        const cards = Array.from(document.querySelectorAll('.card'))
+        return cards.filter((card) => card.style.display !== 'none')
     }
 
     //sets the image src and alt attributes
     function setComicImages(comics) {
         const cards = document.querySelectorAll('.card')
         for (let i = 0; i < comics.length; i++) {
-            cards[i].style.display = ""
+            cards[i].style.removeProperty('display')
             const [title, number, image] = cards[i].children
             title.innerHTML = comics[i].safe_title
             number.innerHTML = `Comic no.${comics[i].num} - year ${comics[i].year}`
@@ -64,7 +58,7 @@
 
     //resets the images on the page
     function resetImages() {
-        const cards = document.querySelectorAll('.card');
+        const cards = document.querySelectorAll('.card')
         for (let card of cards) {
             card.style.display = "none"
         }
@@ -74,35 +68,41 @@
         getComics()
     })
 
-    const nextBtn = document.querySelector('#nxt-btn');
+    const nextBtn = document.querySelector('#nxt-btn')
     nextBtn.addEventListener('click', () => {
+        const displayedImages = getDisplayedImages()
         const [toShow, nextImg] = getParams()
-        getComics(nextImg, toShow)
+        displayedImages.length < toShow ? getComics(nextImg, toShow) : getComics(nextImg+toShow, toShow)
     })
 
     const randBtn = document.querySelector('#rand-btn');
     randBtn.addEventListener('click', () => {
-        const [toShow, nextImg] = getParams('rand')
-        getComics(nextImg, toShow)
+        const randomImg = Math.floor(Math.random() * 2475 + 1)
+        const [toShow, _] = getParams()
+        getComics(randomImg, toShow)
     })
 
     const prevBtn = document.querySelector('#prev-btn');
     prevBtn.addEventListener('click', () => {
-        const [toShow, nextImg] = getParams('prev')
-        getComics(nextImg, toShow)
+        const displayedImages = getDisplayedImages()
+        const [toShow, nextImg] = getParams()
+        displayedImages < toShow ? getComics(nextImg, toShow) : getComics(nextImg-toShow, toShow)
     })
 
     const toShowBtn = document.querySelector('#toShow');
-    toShowBtn.addEventListener('click', () => {
+    toShowBtn.addEventListener('change', () => {
         const url = new URL(window.location.href)
         url.searchParams.set('toShow', toShowBtn.value)
         window.history.pushState({}, '', url)
+        const [toShow, nextImg] = getParams()
+        getComics(nextImg, toShow)
     })
 
     const form = document.querySelector('form')
     form.addEventListener('submit', (ev) => {
         ev.preventDefault()
         const searchInput = parseInt(document.querySelector('#search').value)
+        toShowBtn.value = 1
         getComics(searchInput, 1)
     })
 })()
